33465753cf702b4fbf11f6d81d23f7baa1b22f74
ken <ken90242@gmail.com>
2015-08-20 02:19:32 +0800

    test

--- beta-3.0/test.php
+++ beta-3.0/test.php
@@ -3,7 +3,7 @@
 	session_destroy();	
 ?>
 <html>
-<head>asdsadsasdasdasfffffaaaaaaaaà
+<head>asdsadsasdasdasfffffaaaaaaaaasavvvv
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
 	<script type="text/javascript" src="scripts/iscroll.js"></script>
